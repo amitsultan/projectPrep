a2ab35aeacc6201586dfee62236a5a9ceb380d4a
YardenLevy <49526229+YardenLevy@users.noreply.github.com>
2020-04-17 13:39:26 +0300

    change referee

--- src/main/java/Team/Referee.java
+++ src/main/java/Team/Referee.java
@@ -1,19 +1,57 @@
 package Team;
 
-import Team.Game;
 import Team.LeagueSeasonController;
+import Users.User;
 
 import java.util.HashMap;
 
-public class Referee {
+public class Referee extends User {
 
-    private int ID;
-    String firstName;
-    String lastName;
+
     double salary;
-    private HashMap<String, Game> Games;
     private String type;
     private HashMap<String, LeagueSeasonController> leagueSeasonController;
 
+    public Referee(int ID, String firstName, String lastName, double salary, HashMap<String, Game> games, String type, HashMap<String, LeagueSeasonController> leagueSeasonController) {
 
+        this.salary = salary;
+        this.type = type;
+        this.leagueSeasonController = leagueSeasonController;
+    }
+
+    /**
+     * default constructor
+     */
+    public Referee(){
+        this.ID= 0;
+        this.firstName=null;
+        this.lastName=null;
+
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public HashMap<String, LeagueSeasonController> getLeagueSeasonController() {
+        return leagueSeasonController;
+    }
+
+    public void setLeagueSeasonController(HashMap<String, LeagueSeasonController> leagueSeasonController) {
+        this.leagueSeasonController = leagueSeasonController;
+    }
 }
